09c2cfd3b97adde87b5dbe878c8a7e71b507a113
Andrew Huntington <andrew.huntington@gmail.com>
2022-10-29 19:39:28 +0900

    Refactor todoRoutes

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todoController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get todos" });
-});
-
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Create todo" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update todo ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete todo ${req.params.id}` });
-});
+router.route("/").get(getTodos).post(createTodo);
+router.route("/:id").put(updateTodo).delete(deleteTodo);
 
 module.exports = router;
