3b8380c3d1fb90cfbb76ec557c5ccc057f3f8300
Andrew Huntington <andrew.huntington@gmail.com>
2022-10-31 13:53:30 +0900

    Make all functions in todoController async

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,14 +1,14 @@
 // @desc Get todos
 // @route GET /api/todos
 // @access Private
-const getTodos = (req, res) => {
+const getTodos = async (req, res) => {
   res.status(200).json({ message: "Get todos" });
 };
 
 // @desc Create todo
 // @route POST /api/todos
 // @access Private
-const createTodo = (req, res) => {
+const createTodo = async (req, res) => {
   console.log(req.body);
   res.status(200).json({ message: "Create todo" });
 };
@@ -16,14 +16,14 @@ const createTodo = (req, res) => {
 // @desc Update todo
 // @route PUT /api/todos/:id
 // @access Private
-const updateTodo = (req, res) => {
+const updateTodo = async (req, res) => {
   res.status(200).json({ message: `Update todo ${req.params.id}` });
 };
 
 // @desc Delete todo
 // @route DELETE /api/todos/:id
 // @access Private
-const deleteTodo = (req, res) => {
+const deleteTodo = async (req, res) => {
   res.status(200).json({ message: `Delete todo ${req.params.id}` });
 };
 
