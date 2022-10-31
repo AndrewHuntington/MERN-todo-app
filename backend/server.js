80d3c7a70bdbd782379512c208634375bdb8187d
Andrew Huntington <andrew.huntington@gmail.com>
2022-10-31 13:50:51 +0900

    Add error handling middleware to override Express defaults

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/todos", require("./routes/todoRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
