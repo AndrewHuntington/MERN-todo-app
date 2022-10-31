25b301fbf8bf52c18aa405edab760d5fdd09e06b
Andrew Huntington <andrew.huntington@gmail.com>
2022-10-31 18:24:08 +0900

    Connect to MongoDB with Mongoose

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,31 +1,33 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get todos
 // @route GET /api/todos
 // @access Private
-const getTodos = async (req, res) => {
+const getTodos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get todos" });
-};
+});
 
 // @desc Create todo
 // @route POST /api/todos
 // @access Private
-const createTodo = async (req, res) => {
+const createTodo = asyncHandler(async (req, res) => {
   console.log(req.body);
   res.status(200).json({ message: "Create todo" });
-};
+});
 
 // @desc Update todo
 // @route PUT /api/todos/:id
 // @access Private
-const updateTodo = async (req, res) => {
+const updateTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update todo ${req.params.id}` });
-};
+});
 
 // @desc Delete todo
 // @route DELETE /api/todos/:id
 // @access Private
-const deleteTodo = async (req, res) => {
+const deleteTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete todo ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTodos,
