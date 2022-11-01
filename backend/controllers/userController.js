aab9287aa616bd8cfbbeee99c65942211b5f2cd8
Andrew Huntington <andrew.huntington@gmail.com>
2022-11-01 19:00:07 +0900

    Add user authentication to loginUser in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc Get user data
