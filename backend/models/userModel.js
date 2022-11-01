f79d1840c8bd8711c0dcb22d7b1e0117d0c83edf
Andrew Huntington <andrew.huntington@gmail.com>
2022-11-01 18:41:49 +0900

    Change admin to isAdmin in user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please add a password"],
     },
-    admin: {
+    isAdmin: {
       type: Boolean,
       default: false,
     },
