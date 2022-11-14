4dee288c6d02fa614ab51cdadc39fb0383c1c3e3
Andrew Huntington <andrew.huntington@gmail.com>
2022-11-14 10:26:44 +0900

    Redirect to login from dashboard if there is no user

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../app/hooks";
+
 function Dashboard() {
+  const navigate = useNavigate();
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return <div>Dashboard</div>;
 }
 export default Dashboard;
